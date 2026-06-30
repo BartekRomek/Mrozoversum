0f11a66033bb33afc1b38156adfd9cfa8a01fd0f
Bartek Romek <romekbartek18@gmail.com>
2026-07-01 01:45:40 +0200

    20/06 update

--- data/appearances/index.ts
+++ data/appearances/index.ts
@@ -4,6 +4,10 @@ import zaginiecie from './chylka/zaginiecie.json';
 import rewizja from './chylka/Rewizja.json';
 import ekspozycja from './forst/Ekspozycja.json';
 import przewieszenie from './forst/Przewieszenie.json';
+import trawers from './forst/Trawers.json';
+import immunitet from './chylka/immunitet.json';
+import inwigilacja from './chylka/Inwigilacja.json';
+
 
 
 export const appearancesData = [
@@ -12,4 +16,12 @@ export const appearancesData = [
   ...rewizja.map((char: any) => ({ ...char, bookId: 'rewizja' })),
   ...ekspozycja.map((char: any) => ({ ...char, bookId: 'ekspozycja' })),
   ...przewieszenie.map((char: any) => ({ ...char, bookId: 'przewieszenie' })),
+  ...trawers.map((char: any) => ({ ...char, bookId: 'trawers' })),
+  ...immunitet.map((char: any) => ({ ...char, bookId: 'immunitet' })),
+  ...inwigilacja.map((char: any) => ({ ...char, bookId: 'inwigilacja' })),
+
+
+
+
+
 ];
